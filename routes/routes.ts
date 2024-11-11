8b72661e5515750137e77e500af7c45683f66f80
claGiaSW <claudia.giancola@softwire.com>
2024-11-11 10:51:16 +0000

    test api with sample data

---
+++ routes/routes.ts
@@ -0,0 +1,41 @@
+import express from "express";
+const apiRoutes = express.Router();
+
+interface Toss {
+    wonBy: string;
+    Decision: string;
+}
+
+interface Match {
+    matchID: number;
+    Teams: string;
+    Date: string;
+    Toss: Toss;
+}
+
+const matchDetails: Match[] = [
+  {
+    matchID: 60,
+    Teams: "Rajasthan Royals vs Kolkata Knight Riders",
+    Date: "19 May 2024",
+    Toss: {
+      wonBy: "Team Name",
+      Decision: "TBD",
+    },
+  },
+  {
+    matchID: 61,
+    Teams: "Mumbai Indians vs Chennai Super Kings",
+    Date: "20 May 2024",
+    Toss: {
+      wonBy: "Team Name",
+      Decision: "TBD",
+    },
+  },
+];
+
+apiRoutes.get("/ipl", (req: any, res: any) => {
+  res.status(200).json(matchDetails);
+});
+
+export default apiRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express from "express";
+import apiRoutes from "../routes/routes";
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
-app.use(express.json());
-const router = express.Router();
+// app.use(express.json());
+// const router = express.Router();
 
-router.get('/', (req: any, res: any) => res.send('Hello world !'));
-app.use('/', router);
+// router.get('/', (req: any, res: any) => {
+//   res.send("Hello")
+// });
+
+app.get('/', (req: any, res: any) => {
+  res.send("Hello")
+});
+app.get("/ipl", apiRoutes);
+
+// app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Test backend is running on port ${port}`);
 });
+
+export default app;
